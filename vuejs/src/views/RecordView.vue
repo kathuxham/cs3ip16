b868667c8bd13b6c35c8c3bd9c0df3bd0e8c6f91
kathuxham <115099683+kathuxham@users.noreply.github.com>
2023-01-08 20:25:25 +0000

    Postgresql

---
+++ vuejs/src/views/RecordView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import RecordWindow from "../components/RecordWindow/RecordWindow.vue"
+</script>
+
+<template>
+  <div>
+    <RecordWindow name="testname" id='564564564'/>
+  </div>
+</template>
+
+<style>
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
